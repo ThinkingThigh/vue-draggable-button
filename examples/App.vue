6eed5f3f04b80283c83590df547d81c94c96fd8d
thinkingthigh <thinkingthigh@gmail.com>
2021-05-18 14:43:57 +0800

    feat: run lib and example

--- examples/App.vue
+++ examples/App.vue
@@ -3,7 +3,8 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <draggable-button>
-      <img alt="Vue logo" src="./assets/logo.png">
+      <img alt="Vue logo" @click="clickHandler" src="./assets/logo.png">
+      <div @click="clickHandler">drag it ↑</div>
     </draggable-button>
   </div>
 </template>
@@ -15,7 +16,13 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    clickHandler(){
+      alert("Hi!")
+    }
+  },
+
 }
 </script>
 
